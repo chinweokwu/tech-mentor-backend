dffb05c809dfa8ca754f3ff8b595dfc8714b3e6b
chinweokwu <paulcmorah@gmail.com>
2023-09-12 12:11:07 +0100

    add async to jwt

--- config/jwtToken.js
+++ config/jwtToken.js
@@ -1,9 +1,20 @@
 import jwt from 'jsonwebtoken';
 
-const generateToken= (id) =>{
-  return jwt.sign({id}, process.env.SECRET_KEY, {
-    expiresIn: "1d"
-  });
-}
-export default generateToken;
+const generateToken = (id) => {
+  try {
+    const secretKey = process.env.SECRET_KEY;
+    if (!secretKey) {
+      throw new Error('SECRET_KEY environment variable is missing or empty.');
+    }
 
+    const token = jwt.sign({ id }, secretKey, {
+      expiresIn: "1d"
+    });
+    return token;
+  } catch (error) {
+    console.error("Error generating token:", error);
+    throw new Error("Unable to generate token");
+  }
+};
+
+export default generateToken;
